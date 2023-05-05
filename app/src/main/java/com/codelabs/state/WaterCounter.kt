49eca3b36d0468877770b874dacba6b2fc9f4740
Dario Scoppelletti <dario@scoppelletti.it>
2023-05-05 10:09:13 +0200

    4. Events in Compose

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -2,7 +2,9 @@ package com.codelabs.state
 
 /* ADD-3 - State in Compose */
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,11 +16,25 @@ import androidx.compose.ui.unit.dp
  */
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    /* BEGIN-4 - Events in Compose */
+    // When you run the app and click the button, notice that nothing happens.
+    // Setting a different value for the count variable won't make Compose
+    // detect it as a state change so nothing happens. This is because you
+    // haven't told Compose that it should redraw the screen (that is,
+    // "recompose" the composable function), when the state changes.
+//    val count = 0
+//    Text(
+//        text = "You've had $count glasses.",
+//        modifier = modifier.padding(16.dp)
+//    )
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
+    /* END-4 */
 }
 
 
