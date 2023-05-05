c17ca998182afe8aaf6872cc20b6336fd1066e34
Dario Scoppelletti <dario@scoppelletti.it>
2023-05-05 11:36:33 +0200

    7. Remember in Composition

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -3,6 +3,7 @@ package com.codelabs.state
 /* ADD-3 - State in Compose */
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -79,16 +80,39 @@ fun WaterCounter(modifier: Modifier = Modifier) {
 //        }
 //        /* END-5.3 */
         if (count > 0) {
+            /* BEGIN-7 - Remember in Composition */
+            // remember stores objects in the Composition, and forgets the
+            // object if the source location where remember is called is not
+            // invoked again during a recomposition.
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            /* END-7 */
+
             // This text is present if the button has been clicked
             // at least once; absent otherwise
             Text("You've had $count glasses.")
         }
 
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp),
-            enabled = count < 10) {
-            Text("Add one")
+        /* BEGIN-7 - Remember in Composition */
+//        Button(onClick = { count++ }, Modifier.padding(top = 8.dp),
+//            enabled = count < 10) {
+//            Text("Add one")
+//        }
+//        /* END-6 */
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
-        /* END-6 */
+        /* END-7 */
     }
     /* END-4 */
 }
